f0c724131235e57a68f2f2ad0dd289696b1be01f
Trevor Adams <tadams@tcsiteservices.co.uk>
2015-09-20 21:28:11 +0100

    Initial Commit

---
+++ meme-me-editor-project-1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  meme-me-editor-project-1
+//
+//  Created by Trevor Adams on 20/09/2015.
+//  Copyright © 2015 Trevor Adams. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
