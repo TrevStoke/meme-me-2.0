becbe5023ba85f3d757616f18513cf3b8d575a65
Trevor Adams <tadams@tcsiteservices.co.uk>
2015-09-20 22:53:30 +0100

    Added all required UI elements to MemeEditorViewController storyboard.

--- meme-me-editor-project-1/MemeEditorViewController.swift
+++ meme-me-editor-project-1/MemeEditorViewController.swift
@@ -43,5 +43,9 @@ class MemeEditorViewController: UIViewController, UIImagePickerControllerDelegat
         dismissViewControllerAnimated(true, completion: nil)
     }
 
+    @IBAction func cancelMeme(sender: AnyObject) {
+        pickedImage.image = nil
+        //TODO: cancel text labels
+    }
 }
 
